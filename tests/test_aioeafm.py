68907004df1d321efb5e9297739efecc758cb2f7
John Carr <john.carr@unrouted.co.uk>
2020-02-17 21:33:43 +0000

    Fix tests

--- tests/test_aioeafm.py
+++ tests/test_aioeafm.py
@@ -1,4 +1,7 @@
+import asyncio
+import json
 import pathlib
+from unittest import mock
 
 import aiohttp
 import pytest
@@ -6,20 +9,24 @@ import pytest
 import aioeafm
 
 
-async def mock_response(aiohttp_raw_server, aiohttp_client, path):
-    async def handler(request):
-        with open(pathlib.Path(__file__).parent / "fixtures" / path) as fp:
-            return aiohttp.web.Response(text=fp.read())
+async def mock_session(path):
+    with open(pathlib.Path(__file__).parent / "fixtures" / path) as fp:
+        fixture = json.load(fp)
 
-    raw_server = await aiohttp_raw_server(handler)
-    client = await aiohttp_client(raw_server)
-    return client.session
+    session = mock.Mock()
+    get = session.get.return_value = asyncio.Future()
+
+    response = mock.Mock()
+    get_json = response.json.return_value = asyncio.Future()
+    get_json.set_result(fixture)
+
+    get.set_result(response)
+
+    return session
 
 
-async def test_get_stations(aiohttp_raw_server, aiohttp_client):
-    session = await mock_response(
-        aiohttp_raw_server, aiohttp_client, "get_stations.json"
-    )
+async def test_get_stations():
+    session = await mock_session("get_stations.json")
     stations = await aioeafm.get_stations(session)
 
     station = stations[0]
@@ -30,10 +37,8 @@ async def test_get_stations(aiohttp_raw_server, aiohttp_client):
     assert measures[0]["qualifier"] == "Downstream Stage"
 
 
-async def test_get_station(aiohttp_raw_server, aiohttp_client):
-    session = await mock_response(
-        aiohttp_raw_server, aiohttp_client, "get_station_1491TH.json"
-    )
+async def test_get_station():
+    session = await mock_session("get_station_1491TH.json")
     station = await aioeafm.get_station(session, "1491TH")
 
     assert station["label"] == "Kings Mill"
@@ -41,7 +46,7 @@ async def test_get_station(aiohttp_raw_server, aiohttp_client):
     measures = station["measures"]
     assert measures[0]["parameter"] == "level"
     assert measures[0]["qualifier"] == "Downstream Stage"
-    assert measures[0]["latestReading"]["value"] == 1.885
+    assert measures[0]["latestReading"]["value"] == 1.877
 
 
 @pytest.mark.integration
